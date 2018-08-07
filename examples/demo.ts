889b591f5ed01c8fa6ce8936d61369187efc0e18
Huan LI <zixia@zixia.net>
2018-08-07 13:54:18 +0800

    add demo

---
+++ examples/demo.ts
@@ -0,0 +1,139 @@
+/**
+ *   Wechaty - https://github.com/chatie/wechaty
+ *
+ *   @copyright 2016-2018 Huan LI <zixia@zixia.net>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+// tslint:disable:no-console
+
+import {
+  config,
+  Contact,
+  Message,
+  Wechaty,
+}           from 'wechaty'
+
+import { FileBox }  from 'file-box'
+
+import { PuppetPuppeteer } from '../src/'
+/**
+ *
+ * 1. Declare your Bot!
+ *
+ */
+const puppet = new PuppetPuppeteer()
+
+const bot = new Wechaty({
+  profile : config.default.DEFAULT_PROFILE,
+  puppet,
+})
+
+/**
+ *
+ * 2. Register event handlers for Bot
+ *
+ */
+bot
+.on('logout', onLogout)
+.on('login',  onLogin)
+.on('scan',   onScan)
+.on('error',  onError)
+.on('message', onMessage)
+
+/**
+ *
+ * 3. Start the bot!
+ *
+ */
+bot.start()
+.catch(async e => {
+  console.error('Bot start() fail:', e)
+  await bot.stop()
+  process.exit(-1)
+})
+
+/**
+ *
+ * 4. You are all set. ;-]
+ *
+ */
+
+/**
+ *
+ * 5. Define Event Handler Functions for:
+ *  `scan`, `login`, `logout`, `error`, and `message`
+ *
+ */
+function onScan (qrcode: string, status: number) {
+  // Generate a QR Code online via
+  // http://goqr.me/api/doc/create-qr-code/
+  const qrcodeImageUrl = [
+    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    encodeURIComponent(qrcode),
+  ].join('')
+
+  console.log(`[${status}] ${qrcodeImageUrl}\nScan QR Code above to log in: `)
+}
+
+function onLogin (user: Contact) {
+  console.log(`${user.name()} login`)
+  bot.say('Wechaty login').catch(console.error)
+}
+
+function onLogout (user: Contact) {
+  console.log(`${user.name()} logouted`)
+}
+
+function onError (e: Error) {
+  console.error('Bot error:', e)
+  /*
+  if (bot.logonoff()) {
+    bot.say('Wechaty error: ' + e.message).catch(console.error)
+  }
+  */
+}
+
+/**
+ *
+ * 6. The most important handler is for:
+ *    dealing with Messages.
+ *
+ */
+async function onMessage (msg: Message) {
+  console.log(msg.toString)
+}
+
+/**
+ *
+ * 7. Output the Welcome Message
+ *
+ */
+const welcome = `
+| __        __        _           _
+| \\ \\      / /__  ___| |__   __ _| |_ _   _
+|  \\ \\ /\\ / / _ \\/ __| '_ \\ / _\` | __| | | |
+|   \\ V  V /  __/ (__| | | | (_| | |_| |_| |
+|    \\_/\\_/ \\___|\\___|_| |_|\\__,_|\\__|\\__, |
+|                                     |___/
+
+=============== Powered by Wechaty ===============
+-------- https://github.com/chatie/wechaty --------
+          Version: ${bot.version(true)}
+
+Please wait... I'm trying to login in...
+
+`
+console.log(welcome)
