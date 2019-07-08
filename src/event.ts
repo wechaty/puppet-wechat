9963f8a61daf076a7e0a7fd5f3d8fab96ea2e80d
Huan LI <zixia@zixia.net>
2019-07-08 11:11:39 +0800

    Emit `login` first, then wait `ready` (#97)

--- src/event.ts
+++ src/event.ts
@@ -165,8 +165,11 @@ async function onLogin (
     await this.saveCookie()
     // }
 
-    // fix issue #668
-    await this.waitStable()
+    // fix issue https://github.com/Chatie/wechaty-puppet-puppeteer/issues/107
+    // we do not wait `ready` before emit `login`
+    this.waitStable().catch(e => {
+      log.error('PuppetPuppeteerEvent', 'onLogin() this.waitStable() rejection: %s', e && e.message)
+    })
 
     await this.login(userId)
 
